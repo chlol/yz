9483eff8dcd4d6846f1a4d83f9546746af37f5cf
chlol <chlolz@163.com>
2016-12-05 22:51:14 +0800

    do

--- yeezhuan/src/main/java/com/lnngle/yeezhuan/controller/ContentOutController.java
+++ yeezhuan/src/main/java/com/lnngle/yeezhuan/controller/ContentOutController.java
@@ -1,14 +1,19 @@
 package com.lnngle.yeezhuan.controller;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import com.lnngle.yeezhuan.Consts;
 
 import io.jpress.core.cache.ActionCache;
 import io.jpress.front.controller.ContentController;
+import io.jpress.model.Content;
 import io.jpress.model.User;
 import io.jpress.model.query.UserQuery;
 import io.jpress.router.RouterMapping;
+import io.jpress.template.TemplateManager;
+import io.jpress.template.TplMetadata;
+import io.jpress.template.TplModule;
 import io.jpress.utils.StringUtils;
 
 @RouterMapping(url = Consts.ROUTER_CONTENT)
@@ -18,6 +23,8 @@ public class ContentOutController extends ContentController {
 	public void index() {
 		this.setUserAd();
 		super.index();
+		setMeta();
+		
 	}
 	
 	private void setUserAd() {
@@ -28,5 +35,16 @@ public class ContentOutController extends ContentController {
 			String signature = user.getSignature();
 			this.setAttr("signature", signature);
 		}
+	}
+	
+	public void setMeta() {
+		Content content = this.getAttr("content");
+		TplModule module = TemplateManager.me().currentTemplateModule(content.getModule());
+		List<TplMetadata> metadatas = module.getMetadatas();
+		if (metadatas != null) {
+			for (TplMetadata tplMetadata : metadatas) {
+				setAttr(tplMetadata.getName(), content.metadata(tplMetadata.getName()));
+			}
+		}	
 	}
 }
